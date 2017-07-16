e1c57e92e767c9bd1995af6cb5a9b49214b17382
Christiaan Mes <meschristiaan@gmail.com>
2017-07-16 22:38:41 +0200

    Pulled in debug bar, migration for older mysql versions.

--- config/app.php
+++ config/app.php
@@ -176,6 +176,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Third party packages
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
