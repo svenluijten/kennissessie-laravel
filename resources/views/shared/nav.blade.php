bb2800fdee930595261a650fd30dc8a678b55d1c
Sven Luijten <svenluijten1995@gmail.com>
2017-07-22 20:25:10 +0200

    Add link to posts to nav bar

--- resources/views/shared/nav.blade.php
+++ resources/views/shared/nav.blade.php
@@ -19,7 +19,9 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                &nbsp;
+                <li>
+                    <a href="{{ route('posts.index') }}">Posts</a>
+                </li>
             </ul>
 
             <!-- Right Side Of Navbar -->
