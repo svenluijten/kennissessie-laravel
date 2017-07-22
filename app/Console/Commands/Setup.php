8c808122a01613a1c48e03e85a1bdc63b749c673
Sven Luijten <svenluijten1995@gmail.com>
2017-07-22 15:00:13 +0200

    Introduce 'setup' artisan command

---
+++ app/Console/Commands/Setup.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Setup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set up the application\'s environment for the demo';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->call('migrate:fresh');
+
+        factory('App\User')->create([
+            'name' => 'Sven Luijten',
+            'email' => 'sven@e-sites.nl',
+        ]);
+    }
+}
