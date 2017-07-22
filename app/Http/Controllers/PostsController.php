99e098e6f2c2cfb66f998fab0f39b489ffd11411
Sven Luijten <svenluijten1995@gmail.com>
2017-07-22 16:25:09 +0200

    Order posts by created date

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,7 +9,7 @@ class PostsController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::latest()->get();
 
         return view('posts.index', [
             'posts' => $posts,
