53b7cb38cd5c8eb7881a596f9212b0c2ac8722c3
Sven Luijten <svenluijten1995@gmail.com>
2017-07-22 15:55:45 +0200

    Create post when form is submitted

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -18,6 +19,11 @@ class PostsController extends Controller
 
     public function store(Request $request)
     {
-        //
+        Post::create([
+            'title' => $request->get('title'),
+            'body' => $request->get('body'),
+        ]);
+
+        return redirect()->route('posts.index');
     }
 }
